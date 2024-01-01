78fbfe976fd57be4dbf716f43e07ccd25e0ed577
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2023-12-31 22:20:54 -0700

    Added powershell script for testing

---
+++ test.ps1
@@ -0,0 +1,2 @@
+xcopy C:\Users\explo\git\ChatFeelingsPeck\target\chatfeelings-4.12.1-chilly.jar \\10.0.0.160\minecraft\pufferpanel\lib\servers\77d394a6\plugins\ /y
+rm -r \\10.0.0.160\minecraft\pufferpanel\lib\servers\77d394a6\plugins\ChatFeelings
